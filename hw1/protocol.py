0bc2d553e95e701a124caccbe7a8f1e50364973e
alexander <vashchilko.av@phystech.edu>
2023-11-15 21:46:13 +0300

    init

--- hw1/protocol.py
+++ hw1/protocol.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 
 class UDPBasedProtocol:
@@ -13,15 +14,58 @@ class UDPBasedProtocol:
     def recvfrom(self, n):
         msg, addr = self.udp_socket.recvfrom(n)
         return msg
+    
+
+
+class Packet:
+    ID_LEN = 10
+
+    def __init__(self, data: bytes):
+        self.data = data
+        self.id = os.urandom(self.ID_LEN)
+        self.packet = bytearray()
+        self.packet.extend(self.id)
+        self.packet.extend(data)
+
+    def to_bytes(self):
+        return self.packet
+    
+    def get_data(self):
+        return self.data
+    
+    @classmethod
+    def from_bytes(cls, data: bytes):
+        packet = Packet(b'')
+        packet.id = data[:packet.ID_LEN]
+        packet.data = data[packet.ID_LEN:]
+
+        return packet
 
 
 class MyTCPProtocol(UDPBasedProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.seen_ids = set()
 
     def send(self, data: bytes):
-        return self.sendto(data)
+        packet = Packet(data)
+        packet_bytes = packet.to_bytes()
+        packet_len = len(packet_bytes)
+        for _ in range(5):
+            sent_len = self.sendto(packet_bytes)
+        assert sent_len == packet_len
+
+        return len(data)
 
     def recv(self, n: int):
-        return self.recvfrom(n)
+        while True:
+            packet_bytes = self.recvfrom(4096)
+            packet = Packet.from_bytes(packet_bytes)
+            if packet.id not in self.seen_ids:
+                self.seen_ids.add(packet.id)
 
+                return packet.get_data()#[:n]
+
+
+if __name__ == "__main__":
+    print(Packet.from_bytes(Packet(b'haha').to_bytes()).get_data())
