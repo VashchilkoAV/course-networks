bd94e85c54ba3b458a056c43e6ec5ce8d18d18e6
alexander <vashchilko.av@phystech.edu>
2023-11-18 20:53:48 +0300

    passes

--- hw1/protocol.py
+++ hw1/protocol.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import math
 
 
 class UDPBasedProtocol:
@@ -16,56 +17,102 @@ class UDPBasedProtocol:
         return msg
     
 
+GLOBAL_PACKET_LEN = 10000
+GLOBAL_N_REPEAT = 10
 
 class Packet:
+    TYPE_LEN = 2
     ID_LEN = 10
+    PART_NUM_LEN = 2
+    NUM_PARTS_LEN = 2
+    PACKET_LEN = GLOBAL_PACKET_LEN
+    DATA_LEN = PACKET_LEN - ID_LEN - PART_NUM_LEN - NUM_PARTS_LEN
 
-    def __init__(self, data: bytes):
-        self.data = data
-        self.id = os.urandom(self.ID_LEN)
-        self.packet = bytearray()
-        self.packet.extend(self.id)
-        self.packet.extend(data)
+    def __init__(self, data: bytes, send=True):
+        self.current_part = 0
+        if send == True:
+            self.id = os.urandom(self.ID_LEN)
+            self.num_parts = math.ceil(len(data) / self.DATA_LEN)
+            self.data = [data[i * self.DATA_LEN: (i + 1) * self.DATA_LEN] for i in range(self.num_parts)]
+        else:
+            self.id = data[:self.ID_LEN]
+            self.num_parts = int.from_bytes(data[self.ID_LEN: self.ID_LEN + self.NUM_PARTS_LEN], 'big')
+            current_part = int.from_bytes(data[self.ID_LEN + self.NUM_PARTS_LEN: self.ID_LEN + self.NUM_PARTS_LEN + self.PART_NUM_LEN], 'big')
+            self.data = [None] * self.num_parts
+            self.data[current_part] = data[self.ID_LEN + self.NUM_PARTS_LEN + self.PART_NUM_LEN:]
 
+
+    def __str__(self):
+        return f"id={self.id}, num_parts={self.num_parts}, data={self.data}"
+    
+    def __iter__(self):
+        return self
+    
+    def is_full(self):
+        return all(self.data)
+
+    def __next__(self):
+        if self.current_part < self.num_parts:
+            result = b''.join([
+                self.id,
+                self.num_parts.to_bytes(self.NUM_PARTS_LEN, 'big'),
+                self.current_part.to_bytes(self.PART_NUM_LEN, 'big'),
+                self.data[self.current_part]
+                ])
+            self.current_part += 1
+            
+            return result
+        else:
+            self.current_part = 0
+            raise StopIteration
+        
     def to_bytes(self):
-        return self.packet
-    
-    def get_data(self):
-        return self.data
-    
-    @classmethod
-    def from_bytes(cls, data: bytes):
-        packet = Packet(b'')
-        packet.id = data[:packet.ID_LEN]
-        packet.data = data[packet.ID_LEN:]
-
-        return packet
+        if self.is_full():
+            return b''.join(self.data)
+        else:
+            raise ValueError("It is not permitted to call to_bytes() method when packet is not full\n")
+        
+    def extend_from_bytes(self, data: bytes):
+        id = data[:self.ID_LEN]
+        num_parts = int.from_bytes(data[self.ID_LEN: self.ID_LEN + self.NUM_PARTS_LEN], 'big')
+        current_part = int.from_bytes(data[self.ID_LEN + self.NUM_PARTS_LEN: self.ID_LEN + self.NUM_PARTS_LEN + self.PART_NUM_LEN], 'big')
+        if id == self.id and self.data[current_part] is None:
+            self.data[current_part] = data[self.ID_LEN + self.NUM_PARTS_LEN + self.PART_NUM_LEN:]
+            return True
+        else:
+            return False
 
 
 class MyTCPProtocol(UDPBasedProtocol):
+    N_REPEAT = GLOBAL_N_REPEAT
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.seen_ids = set()
 
     def send(self, data: bytes):
         packet = Packet(data)
-        packet_bytes = packet.to_bytes()
-        packet_len = len(packet_bytes)
-        for _ in range(5):
-            sent_len = self.sendto(packet_bytes)
-        assert sent_len == packet_len
+        for packet_part in packet:
+            for _ in range(self.N_REPEAT):
+                assert self.sendto(packet_part) == len(packet_part)
 
         return len(data)
 
     def recv(self, n: int):
+        # return os.urandom(10)
         while True:
-            packet_bytes = self.recvfrom(4096)
-            packet = Packet.from_bytes(packet_bytes)
+            packet_bytes = self.recvfrom(GLOBAL_PACKET_LEN)
+            packet = Packet(packet_bytes, send=False)
+            
             if packet.id not in self.seen_ids:
                 self.seen_ids.add(packet.id)
-
-                return packet.get_data()#[:n]
-
+                while not packet.is_full():
+                    packet_bytes = self.recvfrom(GLOBAL_PACKET_LEN)
+                    packet.extend_from_bytes(packet_bytes)
+                    
+                return packet.to_bytes()
+                
 
 if __name__ == "__main__":
     print(Packet.from_bytes(Packet(b'haha').to_bytes()).get_data())
